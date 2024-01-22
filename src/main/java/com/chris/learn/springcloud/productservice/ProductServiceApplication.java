3a2b2e99e595e5b77fd8dfdbc9c06298be3c13e8
christin_chacko_qb <christin@qburst.com>
2024-01-23 00:00:36 +0530

    modify to support API gateway

--- src/main/java/com/chris/learn/springcloud/productservice/ProductServiceApplication.java
+++ src/main/java/com/chris/learn/springcloud/productservice/ProductServiceApplication.java
@@ -1,6 +1,5 @@
 package com.chris.learn.springcloud.productservice;
 
-import com.chris.learn.springcloud.productservice.restclients.CouponClient;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
@@ -11,8 +10,8 @@ import org.springframework.cloud.openfeign.EnableFeignClients;
 @EnableFeignClients(basePackages = "com.chris.learn.springcloud.productservice")
 public class ProductServiceApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(ProductServiceApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(ProductServiceApplication.class, args);
+    }
 
 }
--- src/main/java/com/chris/learn/springcloud/productservice/restclients/CouponClient.java
+++ src/main/java/com/chris/learn/springcloud/productservice/restclients/CouponClient.java
@@ -5,7 +5,7 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
-@FeignClient("coupon-service")
+@FeignClient("GATEWAY-SERVICE")
 public interface CouponClient {
     @GetMapping("/api/coupons/{code}")
     CouponDto getCoupon(@PathVariable("code") String code);
